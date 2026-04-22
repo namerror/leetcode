d05fd475f46e91aea353cf50f6b6e9d05ef64589
Leon <leonlong2004@outlook.com>
2026-04-22 13:59:02 -0400

    Add 300

---
+++ 300_Longest_Increasing_Subsequence.py
@@ -0,0 +1,41 @@
+# Accepted, not the best running time complexity ($O(n^2)$). $O(nlogn)$ solutions exist
+
+'''
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+ 
+
+Example 1:
+
+    Input: nums = [10,9,2,5,3,7,101,18]
+    Output: 4
+    Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+
+    Input: nums = [0,1,0,3,2,3]
+    Output: 4
+
+Example 3:
+
+    Input: nums = [7,7,7,7,7,7,7]
+    Output: 1
+
+Constraints:
+
+    1 <= nums.length <= 2500
+    $-10^4$ <= nums[i] <= $10^4$
+'''
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        mx = len(nums)
+        M = [1] * mx
+        for i in range(1, mx):
+            v1 = 1
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    v1 = max(M[j] + 1, v1)
+            M[i] = v1
+
+        return max(M)
